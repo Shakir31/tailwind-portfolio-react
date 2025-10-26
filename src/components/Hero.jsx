c50741243c68b902ae86842e3f5d1bf286eb44d3
Shakir31 <s10269080@connect.np.edu.sg>
2025-10-26 12:35:20 +0800

    updated links

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -14,10 +14,13 @@ const Hero = () => {
             turning ideas into interactive reality
           </p>
           <div className="flex gap-x-4 mt-4">
-            <a href="#">
+            <a href="https://github.com/Shakir31" target="_blank">
               <FaGithubSquare className="h-8 w-8 text-slate-500 hover:text-black duration-300" />
             </a>
-            <a href="#">
+            <a
+              href="https://www.linkedin.com/in/shakir-yunos-363500304/"
+              target="_blank"
+            >
               <FaLinkedin className="h-8 w-8 text-slate-500 hover:text-black duration-300" />
             </a>
           </div>
